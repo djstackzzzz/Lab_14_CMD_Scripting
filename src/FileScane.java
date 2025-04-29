cf2a0a9c40e84572921c0e10ead7266fe60f4826
djstackzzzz <sissokdi@mail.uc.edu>
2025-04-29 14:02:25 -0400

    Initial commit

---
+++ src/FileScane.java
@@ -0,0 +1,9 @@
+public static void main(String[] args) {
+    if (args.length > 0) {
+        String filename = args[0];
+        File file = new File(filename);
+        // Add your file reading and scanning code here
+    } else {
+        // Launch JFileChooser like in Lab 12
+    }
+}
